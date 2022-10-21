46180db00bfe06708efb6062a6ed65a995d25143
Álvaro Gomila Otero <65907634+xalvarxcod@users.noreply.github.com>
2022-10-21 20:53:36 +0200

    Update PassengerRepository.java

--- booking/src/main/java/com/alvaro/booking/repository/PassengerRepository.java
+++ booking/src/main/java/com/alvaro/booking/repository/PassengerRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
-
+// Using MySQL
 @Repository
 public interface PassengerRepository extends JpaRepository<Passenger, Serializable> {
 }
